922a7549d73a1ba2d6bac0125679175a6de36734
Nika990 <109541079+Nika990@users.noreply.github.com>
2022-07-18 20:17:31 +0400

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,103 @@
+// დავალება(1):
+
+//class User {
+//     firstname;
+//     lastname;
+//     age;
+//     studentstatus;
+
+//     constructor(firstname, lastname, age, studentstatus){
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.age = age;
+//         this.studentstatus = studentstatus;
+//     }
+
+//     isUser(){
+//     if (this.user0 == 'user'){
+//         return true;
+//     }
+// }
+
+// isUser(){
+//     if (this.studentstatus == 'user'){
+//         return true
+//     }
+//         return false
+//     }
+
+// }
+
+
+// let user0 = new User('giorgi', 'smith', '25', 'active');
+
+
+// let nika = new User('giorgi')
+
+// console.log(user0);
+
+
+
+
+//დავალება(3):
+
+ //var numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+ //for (var item = 0; item < numbers.length; item++) {
+     //if (numbers[item] > 5) {
+         //console.log(numbers[item]);
+     //}
+ //}
+
+
+
+//დავალება(5):
+
+ //var array = ['watermelon', 'pear', 10, 45, 50,'apple', 'ananas'];
+ //console.log(array[0])
+ //console.log(array[1])
+ //console.log(array[5])
+ //console.log(array[6])
+
+
+
+
+//დავალება(6):
+
+//var array = [32, 10, 'hello', null, 'hello2', 50 ]
+//console.log(array[1])
+//console.log(array[5])
+
+
+
+
+
+
+
+//დავალება(7):
+
+ //var array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ]
+ //console.log(array[0][0])
+ //console.log(array[0][3])
+ //console.log(array[1][0])
+ //console.log(array[1][3])
+ //console.log(array[2][2])
+
+ 
+
+
+
+
+//დავალება(8):
+
+//var array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+//console.log(array[0])
+//console.log(array[8])
+//console.log(array[3])
+//console.log(array[10])
+//console.log(array[5])
+//console.log(array[7])
+//console.log(array[1])
+//console.log(array[2])
+//console.log(array[9])
+//console.log(array[6])
+//console.log(array[4])
