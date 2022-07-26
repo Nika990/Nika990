e7b816f6e59e61929c66d8d69c056b5547b21560
Nika990 <109541079+Nika990@users.noreply.github.com>
2022-07-26 19:21:18 +0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,58 @@
+//დავალება(1):
+
+// let numbers = [2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8];
+
+// for (let index = 0; index < numbers.length; index++) {
+//      const element = numbers[index];
+//      if (element == -9) {
+//         continue;
+//      }
+
+//      if (element == -30) {
+//         continue;
+//      }
+
+//      if (element == -8) {
+//         continue;
+//      }
+
+//      if (element == -11) {
+//         continue;
+//      }
+
+//      if (element == -4) {
+//         continue;
+//      }
+
+//      if (element == -6) {
+//         continue;
+//      }
+
+     
+
+//     console.log(element);
+//  }
+
+
+
+
+
+
+
+
+
+//დავალება(2):
+
+//   function test(x, y) {
+//      if (x, y){
+//           return x + y + x + x + x + x + x;
+//          }
+//         return;
+//   }
+//  let result = test(10, 50, 6, 7, 8, 11, 6, 3, 9);
+//  console.log(result);
+
+
+
+
+ 
