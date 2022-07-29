54162e913290b88416ed67d8abef190cdaaf1f65
Nika990 <109541079+Nika990@users.noreply.github.com>
2022-07-29 19:22:36 +0400

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,122 @@
+//დავალება(1):
+// let numbers = [5, 25, 89, 120, 36];
+//   numbers.shift();
+//   numbers.pop();
+//   console.log(numbers);
+
+
+
+
+//დავალება(2):
+//  let fruits = ['watermelon', 'orange', 'banana', 'mango', 'peach', 'pineapple'];
+//  fruits.shift();
+//  fruits.pop();
+//  console.log(fruits);
+
+
+
+
+
+//დავალება(3):
+// let numbers = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArray = numbers.map( y => y / 3)
+// console.log(newArray)
+
+
+
+
+
+//დავალება(4):
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+// let newArray = array.filter(function(y) {
+//     return y > 0;
+// })
+// console.log(newArray);
+
+
+
+
+//დავალება(5):
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+// let newArray = languages.filter(function(item) {
+//          return item.length > 3;
+//      })
+//      console.log(newArray);
+
+
+
+
+//დავალება(6):
+// let array1 = ['academy'];
+// let array2 = ['of'];
+// let array3 = ['digital'];
+// let array4 = ['industries'];
+// let newValue = array1.concat(array2,array3,array4);
+// console.log(newValue)
+
+
+
+//დავალება(8):
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+//  let newArray = words.filter(function(item) {
+//           return item.length < 6;
+//      })
+//      console.log(newArray);
+
+
+
+
+//დავალება(9):
+// let array1 = [1, 2];
+// let array2 = [3, 4];
+// let array3 = [5, 6];
+// let newValue = array1.concat(array2,array3);
+// console.log(newValue)
+
+
+
+
+//დავალება(11):
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+// let newArray = numbers.filter(function(y) {
+//      return y > 0;
+//      })
+//      console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
